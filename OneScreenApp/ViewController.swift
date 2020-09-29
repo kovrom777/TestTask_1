df9d6ada85ccbc123ea07e9300ea5d8a2b8ee27c
Roman Kovayin <r.kovaykin@avsw.ru>
2020-09-29 13:00:35 +0300

    Initial Commit

---
+++ OneScreenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneScreenApp
+//
+//  Created by Роман Ковайкин on 29.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
